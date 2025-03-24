063ebf4f06c482a3e5f133d56ca7d94dc198fbba
Sebastian <ahss@aarhus.dk>
2025-03-24 08:53:19 +0100

    Don't send error mail on business exception

--- robot_framework/exceptions.py
+++ robot_framework/exceptions.py
@@ -39,7 +39,8 @@ def handle_error(message: str, error_count: str | None, error: Exception, queue_
     orchestrator_connection.log_error(error_msg)
     if queue_element:
         orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.FAILED, error_msg)
-    error_screenshot.send_error_screenshot(error_email, error, orchestrator_connection.process_name)
+    if error != BusinessError:    
+        error_screenshot.send_error_screenshot(error_email, error, orchestrator_connection.process_name)
 
     if message == "ApplicationException" and error_count == config.MAX_RETRY_COUNT:
         try:
